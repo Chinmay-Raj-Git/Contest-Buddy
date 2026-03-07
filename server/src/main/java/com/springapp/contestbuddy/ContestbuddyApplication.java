f74ec7c4110cf73d1f27c0df5fb47c73448b3b7c
Chinmay-Raj-Git <ychinmayraj06@gmail.com>
2026-03-07 11:12:03 +0530

    Added SpringBoot Structure

---
+++ server/src/main/java/com/springapp/contestbuddy/ContestbuddyApplication.java
@@ -0,0 +1,13 @@
+package com.springapp.contestbuddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContestbuddyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContestbuddyApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/springapp/contestbuddy/ContestbuddyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springapp.contestbuddy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContestbuddyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
